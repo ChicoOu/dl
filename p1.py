3fb8caa85d7e1a8be5c0152a5eda9aade5216c15
Chico <16483314@qq.com>
2018-10-30 21:26:34 +0800

    Added hist vis

--- p1.py
+++ p1.py
@@ -83,7 +83,8 @@ def Train(m):
     m.compile(optimizer=tf.train.AdamOptimizer(0.001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-    m.fit(data, labels, epochs=10, batch_size=32, validation_data=(test_data, test_labels))
+    hist = m.fit(data, labels, epochs=10, batch_size=32, validation_data=(test_data, test_labels))
+    TrainingVis(hist)
     m.save(MODEL_FILE)
 
 def ReadTest():
@@ -96,6 +97,31 @@ def Test(m):
     test = ReadTest()
     m.predict(test, batch_size=30)
 
+def TrainingVis(hist):
+    loss = hist.history['loss']
+    val_loss = hist.history['val_loss']
+    acc = hist.history['acc']
+    val_acc = hist.history['val_acc']
+
+    # make a figure
+    fig = plt.figure(figsize=(8,4))
+    # subplot loss
+    ax1 = fig.add_subplot(121)
+    ax1.plot(loss,label='train_loss')
+    ax1.plot(val_loss,label='val_loss')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss')
+    ax1.set_title('Loss on Training and Validation Data')
+    ax1.legend()
+    # subplot acc
+    ax2 = fig.add_subplot(122)
+    ax2.plot(acc,label='train_acc')
+    ax2.plot(val_acc,label='val_acc')
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Accuracy')
+    ax2.set_title('Accuracy  on Training and Validation Data')
+    ax2.legend()
+    plt.tight_layout()
 
 if __name__ == '__main__':
     m = Models()
